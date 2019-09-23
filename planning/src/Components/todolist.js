4e22b697beea9c7ce2641fdd77ae7b3fd93de07f
strimnal32246 <susan.trimnal@icdccollege.edu>
2019-09-23 19:45:36 -0400

    fixed bug

--- planning/src/Components/todolist.js
+++ planning/src/Components/todolist.js
@@ -4,7 +4,7 @@ import Todo from "../todo";
 const TodoList = props => {
   return (
     <div className="todo-list">
-      <h1>Susan ToDo List:</h1>
+      <h1> To Do List:</h1>
       {props.data.map(item => (
         <Partiestodo
           dispatch={props.dispatch}
