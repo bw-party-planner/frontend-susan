d24eb468f728ad79194c874e3c8a45f0a9ee6520
AjileJ <53097022+AjileJ@users.noreply.github.com>
2019-09-23 13:38:08 -0700

    login and signup form still needs to be put in signup page

--- planning/src/App.js
+++ planning/src/App.js
@@ -1,21 +1,15 @@
 import React from 'react';
-import {Party, SignUp,ShoppingList,TodoList,Pictures,Navigation}
+import SignUpForm from './Components/SignUpForm';
 import './App.css';
-import {Route} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <h1>hi</h1>
-       <Navigation />
-       <Route exact path ='/' component={Party}/>
-       <Route path = '/sign-up' component={SignUp}/>
-       <Route path = '/Shopping-list' component={ShoppingList}/>
-       <Route path = '/todo-list' component={TodoList}/>
-       <Route path = '/pictures' component={Pictures}/>
-      </header>
+       <h1>Sign-up/ Log-in</h1>
+       <SignUpForm />
     </div>
+    
   );
 }
 
---
+++ planning/src/Components/SignUpForm.js
@@ -0,0 +1,67 @@
+import React, {useState, useEffect} from 'react';
+import {withFormik, Form, Field, Formik} from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
+
+const SignUpForm = ({values,errors,touched,status}) => {
+  const [users, setUsers] = useState([]);
+  useEffect(()=> {
+    if(status) {
+      setUsers ([...users, status]);
+    }
+  },[status]);
+  return(
+    <div className="user-form">
+      <Form>
+        <Field type='text' name='name' placeholder='Name'/>
+        {touched.name && errors.name && (
+          <p className='error'>{errors.name}</p>
+        )}
+        <Field type = 'text' name='email' placeholder='Email'/>
+        {touched.email && errors.email && (
+          <p className='error'>{errors.email}</p>
+        )}
+        <Field type = 'text' name='password' placeholder='Password'/>
+        {touched.password && errors.password && (
+          <p className='error'>{errors.password}</p>
+        )}
+        <button>Sign Up</button>
+      </Form>
+      {users.map(user => (
+        <ul key={user.id}>
+          <li>Name: {user.name}</li>
+          <li>Email: {user.email}</li>
+          <li>Password: {user.password}</li>
+        </ul>
+      ))}
+    </div>
+  )
+}
+
+const FormikSignUpForm = withFormik ({
+  mapPropsToValues({name, email, password}){
+    return {
+      name: name || '',
+      email: email || '',
+      password: password || ''
+     
+    };
+  },
+  validationSchema:Yup.object().shape({
+    name: Yup.string().required("Your name is required here!!!"),
+    email: Yup.string().required("Your email is required here!!!"),
+    password: Yup.string().required("Please insert a Password!!!!"),
+    
+  }),
+  handleSubmit(values,{setStatus, resetForm}){
+    axios
+    .post('https://reqres.in/api/users', values)
+    .then(res => {
+      setStatus(res.data);
+      resetForm();
+      
+    })
+    .catch(err => console.log(err.res))
+  }
+})(SignUpForm);
+export default FormikSignUpForm
