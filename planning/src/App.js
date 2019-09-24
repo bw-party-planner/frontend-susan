eb7beef1e00cb5ba3091940b58141a57ebad975a
strimnal32246 <susan.trimnal@icdccollege.edu>
2019-09-23 22:39:56 -0400

    had to delete a few things to help app for now will add back later

--- planning/src/App.js
+++ planning/src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import { Route, Router } from "react-router-dom";
 import "./App.css";
-import ShoppingCart from "./Components/ShoppingCart.js";
+
 
 import { PartyContext } from "./Contexts/PartyContext.js";
 import { CartContext } from "./Contexts/CartContext.js";
@@ -10,17 +9,14 @@ import parties from "./parties/parties.js";
 function App() {
   const [cart, setCart] = useState([]);
 
-  return (
-    <Router>
+  return(
       <div className="App">
         <PartyContext.Provider>
           <CartContext.Provider value={cart}>
             <header className="App-header"></header>
-            <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
           </CartContext.Provider>
         </PartyContext.Provider>
       </div>
-    </Router>
   );
 }
 
