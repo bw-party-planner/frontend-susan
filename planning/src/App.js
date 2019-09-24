7212165a30bbf07f0bd9efcad08d23116bcb4ec0
strimnal32246 <susan.trimnal@icdccollege.edu>
2019-09-23 22:22:57 -0400

    added pictures and picture context

--- planning/src/App.js
+++ planning/src/App.js
@@ -2,8 +2,12 @@ import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import "./App.css";
 import ShoppingCart from "./Components/ShoppingCart.js";
-import  {PartyContext}  from "./Contexts/PartyContext.js";
-import  {CartContext}  from "./Contexts/CartContext.js";
+import PrivateRoute from "./Components/PrivateRoute.js";
+
+import { PartyContext } from "./Contexts/PartyContext.js";
+import { CartContext } from "./Contexts/CartContext.js";
+
+import parties from "./parties/parties.js";
 function App() {
   const [cart, setCart] = useState([]);
 
@@ -13,6 +17,7 @@ function App() {
         <CartContext.Provider value={cart}>
           <header className="App-header"></header>
           <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+          {<PrivateRoute exact path="/parties" component={parties} />}
         </CartContext.Provider>
       </PartyContext.Provider>
     </div>
--- planning/src/parties/partiespic.js
+++ planning/src/parties/partiespic.js
@@ -1,7 +1,22 @@
 import React from "react";
-class Pic extends React.Component {
-  constructor() {
-    super();
-    this.newValue = {};
-  }
-}
+import { useContext } from "react";
+import { PicturesContext } from "./contexts/PicturesContext.js";
+
+const Pictures = () => {
+  const { pictures } = useContext(PicturesContext);
+
+  return (
+    <div>
+      <h1>Hello Welcome To Pictures!</h1>
+      {pictures.map(pic => (
+        <div>
+          <h1>{pic.party}</h1>
+          <h2>{pic.partyid}</h2>
+          <h2>{pic.url}</h2>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Pictures;
