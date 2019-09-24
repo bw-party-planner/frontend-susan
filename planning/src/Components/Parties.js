c9c8c52498dbf503a8fa854ae2af2ed7d9c30590
AjileJ <53097022+AjileJ@users.noreply.github.com>
2019-09-24 10:00:45 -0700

    mock category form

--- planning/src/Components/Parties.js
+++ planning/src/Components/Parties.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
+import CategoryForm from './CategoryForm';
 import '../index.css';
 
 
@@ -11,7 +12,7 @@ background: purple;
 const PartyT = styled.h1`
 margin:0;
 padding-top: 1%;
-padding-bottom: 1%
+padding-bottom: 1%;
 `;
 
 const PartyP = styled.p`
@@ -26,6 +27,7 @@ return(
     <PartyP>
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, accusantium? Necessitatibus aliquam sit incidunt qui cumque nam cupiditate ullam corporis eveniet, velit mollitia ad praesentium. Porro cupiditate ipsum nesciunt consequatur.
     </PartyP>
+    <CategoryForm/>
   </div>
   </PartyB>
 );
