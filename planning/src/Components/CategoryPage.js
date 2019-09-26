a9b5ba9f008d785e128a4f13fbb8d0bdd25b05e4
strimnal32246 <susan.trimnal@icdccollege.edu>
2019-09-26 13:05:38 -0400

    merging deleted some things

--- planning/src/Components/CategoryPage.js
+++ planning/src/Components/CategoryPage.js
@@ -1,24 +1,23 @@
-import React, {useState,useEffect} from 'react';
-import axios from 'axios';
-
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function CategoryPage() {
   const [infos, setInfos] = useState([]);
-  useEffect(()=> {
-    axios.get('https://mypartyplanner.herokuapp.com/api/categories')
-    .then(response => {
-      const info = response.data;
-      console.log(response.data);
-      setInfos(info);
-    })
-    .catch(error => {
-     console.log("there is an error with axios", error);
-    });
-  },[]);
+  useEffect(() => {
+    axios
+      .get("https://mypartyplanner.herokuapp.com/api/categories")
+      .then(response => {
+        const info = response.data;
+        console.log(response.data);
+        setInfos(info);
+      })
+      .catch(error => {
+        console.log("there is an error with axios", error);
+      });
+  }, []);
   return (
     <div>
-      <h1>jnjkn</h1>
+      <h1>Welcome</h1>
     </div>
-  )
+  );
 }
